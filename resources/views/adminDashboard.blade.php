4819f4dd22a32c870b1d680b4ecd02c2b6d9d567
Red Aaron Negrite <redaaronpnegrite@iskolarngbayan.pup.edu.ph>
2025-12-18 18:42:43 +0800

    FEAT: Added Admin Sidebar

---
+++ resources/views/adminDashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('base')
+
+@section('title', 'Caffe Arabica - Dashboard')
+
+@section('content')
+    <div class="flex min-h-screen bg-gray-100">
+        <!-- Sidebar -->
+        @include('admin.adminSidebar')
+
+        <!-- Main Content Area -->
+        <div class="flex-1 flex flex-col">
+
+        </div>
+    </div>
+@endsection
